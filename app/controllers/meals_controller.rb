ef741b58354c35c12eaee7a9c43e467766946914
Rob Rowe <gizmo.hacker585@4wrd.cc>
2021-05-31 17:09:37 +0100

    Create routes, meals controller, basic html

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,32 @@
+class MealsController < ApplicationController
+  # before_action :set_meal, only: [:show]
+
+  def index
+    @meals = Meal.all
+  end
+
+  def show; end
+
+  def new
+    @meal = Meal.new
+  end
+
+  def create
+    @meal = Meal.new(meal_params)
+    if @meal.save
+      redirect_to @meal
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_meal
+    @meal = Meal.find(params[:id])
+  end
+
+  def meal_params
+    params.require(:meal).permit(:name, :description, :price)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/meals', to: 'meals#index'
+  get '/meals/new', to: 'meals#new', as: 'new_meal'
+  post '/meals', to: 'meals#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

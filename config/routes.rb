4cfd069b114ec40abed7bb5ffbd0ccb06e2298e9
Egidio De-Souza <egidio.de-souza@hotmail.co.uk>
2021-06-01 16:23:13 +0100

    Access to show-page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/meals', to: 'meals#index'
   get '/meals/new', to: 'meals#new', as: 'new_meal'
   post '/meals', to: 'meals#create'
+  get '/meals/:id', to: 'meals#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

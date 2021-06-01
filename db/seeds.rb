f216f386215d3f85c2aa150bc99cb844bb5d3ee6
Rob Rowe <gizmo.hacker585@4wrd.cc>
2021-06-01 12:17:37 +0100

    Add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,31 @@
+DatabaseCleaner.clean_with(:truncation)
+
 # generate users
 12.times do
   email = Faker::Internet.email
   password = 'changeme'
-  # name = Faker::Name.name
   puts "Creating user account for #{email}"
   User.create!(email: email, password: password)
-  # , name: name
-  puts "User ##{User.id} created"
+  puts "User created"
 end
 
 # generate meals
 50.times do
-
+  name = Faker::Food.dish
+  description = Faker::Food.description
+  price = "#{rand(1..24)}.99"
+  chef = User.find(rand(1..12))
+  puts "Creating #{name}, from #{chef.email}"
+  Meal.create!(name: name, description: description, price: price, chef: chef)
+  puts "Meal created"
 end
 
 # generate purchases
 30.times do
-
+  quantity = rand(1..5)
+  meal = Meal.find(rand(1..50))
+  buyer = User.find(rand(1..12))
+  puts "Creating purchase of #{quantity}x #{meal.name} from user #{buyer.email}"
+  Purchase.create!(meal: meal, buyer: buyer, quantity: quantity)
+  puts "Purchase created"
 end

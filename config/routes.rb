1cb26589a335235f46c213f937a1bd862c28a329
Rob Rowe <gizmo.hacker585@4wrd.cc>
2021-06-03 17:32:56 +0100

    create user account dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get '/meals/:id/purchases/new', to: 'purchases#new' , as: 'new_purchase'
   post '/meals/:id/purchases', to: 'purchases#create', as:   'create_purchase'
   get '/purchases', to: 'purchases#index'
+  get '/users/edit', to: 'users#edit', as: 'user_edit'
+  patch '/users', to: 'users#update'
 
   resources :purchases, only: :destroy
 

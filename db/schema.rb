546fd45576c7078650be9bda96b65ccc45afe79f
Rob Rowe <gizmo.hacker585@4wrd.cc>
2021-06-04 16:56:35 +0100

    images

--- db/schema.rb
+++ db/schema.rb
@@ -68,6 +68,8 @@ ActiveRecord::Schema.define(version: 2021_06_04_140728) do
     t.string "first_name"
     t.string "last_name"
     t.boolean "chef"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
